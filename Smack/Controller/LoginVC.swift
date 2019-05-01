502f1cb6fe062bb1e90278c76e74cd0c9270bc7f
Tyler Cantlon <Tyler@Tyler-Cantlons-MacBook.local>
2019-05-01 18:34:55 -0500

    first commit of Login VC UI

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Tyler Cantlon on 5/1/19.
+//  Copyright © 2019 Tyler. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Tyler Cantlon on 5/1/19.
+//  Copyright © 2019 Tyler. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
